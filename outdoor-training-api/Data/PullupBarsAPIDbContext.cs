d17eb3ad5b5180ad244a9833aaea836cb519dfba
sebastian-meckovski <sebastian.meckovski@gmail.com>
2024-03-18 19:58:03 +0100

    Revert "login and register endpoints added"
    This reverts commit 09529dd12a15070a6aedd964b1d990fc920638a1.

--- outdoor-training-api/Data/PullupBarsAPIDbContext.cs
+++ outdoor-training-api/Data/PullupBarsAPIDbContext.cs
@@ -1,35 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PullupBars.Models;
 
-
 namespace PullupBars.Data
 {
-    public class PullupBarsAPIDbContext : IdentityDbContext
+    public class PullupBarsAPIDbContext : DbContext
     {
-
         public PullupBarsAPIDbContext(DbContextOptions options) : base(options)
         {
 
         }
-
         public DbSet<PullUpBar> PullupBars { get; set; }
     }
 }
-
-
-
-
-// public class ApplicationDbContext : IdentityDbContext  
-// {  
-//     public DbSet<ApplicationUser> ApplicationUsers { get; set; }  
-//     public DbSet<ApplicationRole> ApplicationRoles { get; set; }  
-//     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)  
-//         : base(options)  
-//     {  
-//     }  
-//     protected override void OnModelCreating(ModelBuilder builder)  
-//     {  
-//         base.OnModelCreating(builder);   
-//     }  
-// }  
